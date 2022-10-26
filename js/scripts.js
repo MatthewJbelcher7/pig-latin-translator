d52963267fec7e87ab2eb82d30d74ba6a4ac2a8e
Alejandro Socarras <apsocarras@gmail.com>
2022-10-26 17:13:47 -0600

    Complete first two tests of pigLatin()
    Co-authored-by: Matthew Belcher <Matthewjbelcher7@live.com>
    
    Co-authored-by: Auston Moos <Austonhmoos2004@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,23 @@
+// Business Logic
+
+function pigLatin(text) {
+  // e.g. text = "sample"
+  // sample[0] = "s", sample[1] = "a", etc. 
+  const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]; 
+  if (vowels.includes(text[0])){
+    return text + "way";
+  } else {
+    return text.slice(1) + text[0] + "ay";
+  }
+}
 // UI Logic 
 
-// Business Logic
+
+
+
+
+
+// 
+// String.prototype.slice()
+// String.prototype.includes()
+// String.prototype.indexOf()
